35b489b5bd8f0ba3870247a0c8e04c4b516a4f3e
Kaleb Petersen <kcpetersen@LAPTOP-6C7T2E9V.localdomain>
2021-11-12 10:40:25 -0700

    pre bio lab

--- pxlart/pixel.cpp
+++ pxlart/pixel.cpp
@@ -1,48 +1,48 @@
 #include "pixel.h"
 
-pixel::pixel(QWidget *parent) : QWidget(parent), pxlRed(0), pxlGreen(0), pxlBlue(0), pxlSize(20){
+Pixel::Pixel(QWidget *parent) : QWidget(parent), pxlRed(0), pxlGreen(0), pxlBlue(0), pxlSize(20){
 
 }
-int pixel::getRed(){
+int Pixel::getRed() const{
 
 	return pxlRed;
 
 }
 
-int pixel::getGreen(){
+int Pixel::getGreen() const{
 
 	return pxlGreen;
 
 }
 
-int pixel::getBlue(){
+int Pixel::getBlue() const{
 
 	return pxlBlue;
 
 }
 
-void pixel::setRed( const int& red ){
+void Pixel::setRed( const int& red ){
 
 	if(red >= 0 && red <= 255){
 		pxlRed = red;
 	}
 }
 
-void pixel::setGreen( const int& green ){
+void Pixel::setGreen( const int& green ){
 
 	if(green >= 0 && green <= 255){
 		pxlGreen = green;
 	}
 }
 
-void pixel::setBlue( const int& blue ){
+void Pixel::setBlue( const int& blue ){
 
 	if(blue >= 0 && blue <= 255){
 		pxlBlue = blue;
 	}
 }
 
-void pixel::setSize( const int& size ){
+void Pixel::setSize( const int& size ){
 
 	if(size >= 0 && size <= 80){ //Does this need to be changed?
 		pxlSize = size;

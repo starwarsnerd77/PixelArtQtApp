9ca6af658ed18263bdf8633b2ec540eb86986426
Brendan Davidson <binary141@gmail.com>
2021-11-12 12:36:26 -0700

    main file with goodies

--- pxlart/main.cpp
+++ pxlart/main.cpp
@@ -1,23 +1,61 @@
 #include "mainwindow.h"
-
 #include <QApplication>
-#include <QLocale>
-#include <QTranslator>
+
+#include <QMainWindow>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QLabel>
+#include <QLineEdit>
+#include <QGroupBox>
+
+#include <QTextEdit>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "pxlart_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
-    MainWindow w;
-    w.show();
+    QMainWindow window;
+    QWidget *widget = new QWidget(&window);
+    QGridLayout *layout = new QGridLayout(widget);
+
+    window.setCentralWidget(widget);
+    widget->setLayout(layout);
+
+    QGroupBox *box = new QGroupBox("", widget);
+    layout->addWidget(box, 0, 0);
+
+    QVBoxLayout *boxLayout = new QVBoxLayout(box);
+
+    QWidget* nameWidget = new QWidget(box);
+    QWidget* ageWidget = new QWidget(box);
+    QWidget* addressWidget = new QWidget(box);
+
+    boxLayout->addWidget(nameWidget);
+    boxLayout->addWidget(ageWidget);
+    boxLayout->addWidget(addressWidget);
+
+    QHBoxLayout *nameLayout = new QHBoxLayout(nameWidget);
+    nameLayout->addWidget(new QLabel("Name:"));
+    nameLayout->addWidget(new QLineEdit(nameWidget));
+
+
+    QHBoxLayout *ageLayout = new QHBoxLayout(ageWidget);
+    ageLayout->addWidget(new QLabel("Age:"));
+    ageLayout->addWidget(new QLineEdit(ageWidget));
+
+    QHBoxLayout *addressLayout = new QHBoxLayout(addressWidget);
+    addressLayout->addWidget(new QLabel("Address:"));
+    addressLayout->addWidget(new QLineEdit(addressWidget));
+
+
+
+    layout->addWidget(new QPushButton("Validate", widget), 1, 0);
+    layout->addWidget(new QPushButton("Reset", widget), 1, 1);
+    layout->addWidget(new QPushButton("Cancel", widget), 1, 2);
+
+
+    window.show();
+
     return a.exec();
 }
